8de803081db6a3219c827ddb58a5ede9bf5d5496
PaulPowershell <116181531+PaulPowershell@users.noreply.github.com>
2023-10-09 14:04:08 +0200

    add files

---
+++ go.mod
@@ -0,0 +1,27 @@
+module main
+
+go 1.20
+
+require (
+	github.com/Azure/azure-sdk-for-go v68.0.0+incompatible
+	github.com/Azure/go-autorest/autorest/azure/auth v0.5.12
+	github.com/manifoldco/promptui v0.9.0
+)
+
+require (
+	github.com/Azure/go-autorest v14.2.0+incompatible // indirect
+	github.com/Azure/go-autorest/autorest v0.11.24 // indirect
+	github.com/Azure/go-autorest/autorest/adal v0.9.18 // indirect
+	github.com/Azure/go-autorest/autorest/azure/cli v0.4.5 // indirect
+	github.com/Azure/go-autorest/autorest/date v0.3.0 // indirect
+	github.com/Azure/go-autorest/autorest/to v0.4.0 // indirect
+	github.com/Azure/go-autorest/autorest/validation v0.3.1 // indirect
+	github.com/Azure/go-autorest/logger v0.2.1 // indirect
+	github.com/Azure/go-autorest/tracing v0.6.0 // indirect
+	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e // indirect
+	github.com/dimchansky/utfbom v1.1.1 // indirect
+	github.com/golang-jwt/jwt/v4 v4.2.0 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	golang.org/x/crypto v0.0.0-20211215153901-e495a2d5b3d3 // indirect
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
+)
---
+++ ksub.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"regexp"
+	"sort"
+	"strings"
+
+	"github.com/Azure/azure-sdk-for-go/profiles/latest/resources/mgmt/subscriptions"
+	"github.com/Azure/go-autorest/autorest/azure/auth"
+	"github.com/manifoldco/promptui"
+)
+
+func main() {
+	authorizer, err := auth.NewAuthorizerFromCLI()
+	if err != nil {
+		log.Fatalf("Failed to create Azure authorizer: %v", err)
+		return
+	}
+
+	subscriptionsClient := subscriptions.NewClient()
+	subscriptionsClient.Authorizer = authorizer
+
+	ctx := context.Background()
+
+	result, err := subscriptionsClient.List(ctx)
+	if err != nil {
+		log.Fatalf("Failed to retrieve subscriptions: %v", err)
+		return
+	}
+
+	subscriptionNames := make([]string, len(result.Values()))
+
+	for i, subscription := range result.Values() {
+		subscriptionNames[i] = *subscription.DisplayName
+	}
+
+	sort.Strings(subscriptionNames)
+
+	prompt := promptui.Select{
+		Label: "Select a subscription",
+		Items: subscriptionNames,
+		Templates: &promptui.SelectTemplates{
+			Label:    "{{ . }}",
+			Active:   "\U000027A4 {{ . | cyan | bold }}",
+			Inactive: "  {{ . }}",
+		},
+		Size:         50,
+		HideSelected: true,
+	}
+
+	_, selected, err := prompt.Run()
+	if err != nil {
+		log.Println("Failed to prompt selection", err)
+		return
+	}
+
+	setSubscription(selected)
+	setKubernetesContext(selected)
+}
+
+func setSubscription(subscriptionID string) error {
+	// Check if the subscription ID is provided
+	if subscriptionID == "" {
+		return fmt.Errorf("subscription ID is required")
+	}
+
+	// Create the command to change the Azure subscription context
+	cmd := exec.Command("az", "account", "set", "--subscription", subscriptionID)
+
+	// Set the command's output to os.Stdout and os.Stderr
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	// Execute the command
+	err := cmd.Run()
+	if err != nil {
+		return fmt.Errorf("failed to change Azure subscription context: %v", err)
+	} else {
+		fmt.Println("✔ Switched to subscription", subscriptionID)
+	}
+	return nil
+}
+
+func setKubernetesContext(selectedID string) {
+	subscriptionName := selectedID
+
+	re := regexp.MustCompile(`\d{6}`)
+	lot := re.FindString(subscriptionName)
+
+	cmd := exec.Command("kubectx")
+	output, err := cmd.Output()
+	if err != nil {
+		fmt.Printf("Failed to execute 'kubectx' command: %v", err)
+		return
+	}
+
+	kubecontext := ""
+	lines := strings.Split(string(output), "\n")
+	for _, line := range lines {
+		if strings.Contains(line, lot) {
+			kubecontext = strings.TrimSpace(line)
+			break
+		}
+	}
+
+	if kubecontext != "" {
+		cmd = exec.Command("kubectx", kubecontext)
+		err = cmd.Run()
+		if err != nil {
+			fmt.Printf("Failed to switch to kubecontext: %v", err)
+			return
+		} else {
+			fmt.Println("✔ Switched to cluster", kubecontext)
+		}
+	} else {
+		fmt.Println("❌ Pas de cluster associé")
+	}
+}
